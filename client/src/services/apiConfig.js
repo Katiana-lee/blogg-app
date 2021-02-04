4ae3c74ed9c90e147c3aa16bbe762eb14034a3ea
Grady King <grady.king07@gmail.com>
2021-02-04 15:02:27 -0500

    front-end data rendering

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -4,9 +4,10 @@ let apiUrl
 
 const apiUrls = {
     production: 'https://blog-app-group-2.herokuapp.com/api',
-    development: 'http://localhost:3000/api'
+    // development: 'http://localhost:3000/api'
+  development: 'https://blog-app-group-2.herokuapp.com/api'
 }
-
+console.log(window.location.hostname)
 if (window.location.hostname === 'localhost') {
     apiUrl = apiUrls.development
 } else {
