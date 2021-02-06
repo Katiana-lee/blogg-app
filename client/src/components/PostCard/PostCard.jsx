4697d1ad18086a21e18b6cfba9db7c21981d464b
Katiana <katiana002@gmail.com>
2021-02-06 18:07:15 -0500

    debuged PostCard issues

--- client/src/components/PostCard/PostCard.jsx
+++ client/src/components/PostCard/PostCard.jsx
@@ -4,22 +4,20 @@ import { Link } from 'react-router-dom'
 import profilePic from './blankProfile.png';
 
 const PostCard = (props) => {
+  console.log('this is ', props)
   console.log('this is ')
   console.log(props)
   let timestamp = "2021-02-04T21:39:32.009Z";
-  
+
   const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   let dateString = months[parseInt(timestamp.slice(5, 7)) - 1];
   dateString += " " + parseInt(timestamp.slice(8, 10));
   dateString += ", " + parseInt(timestamp.slice(0, 4))
-  
+
   return (
     <div className="post-card">
       <Link className="card" to={`/posts/${props.id}`}>
-<<<<<<< HEAD
-=======
-        <img className="post-card-image" src={props.imgURL} alt={props.title} />
->>>>>>> a7fe682e72eb6cd862ad2780c1afa7fc94de164b
+        
         <div className="blog-title">{props.title}</div>
         <div className="userInfo">
           <div className="dateString">{dateString}</div>
@@ -33,8 +31,7 @@ const PostCard = (props) => {
         </div>
         <div className="content">{props.content}</div>
     </Link>
-</div>
+    </div>
   )
-}
-
-export default PostCard;
+  }
+  export default PostCard;
